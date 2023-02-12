da6bfb0d8700251a2d86110eb4d5c3c6950d16c8
gustavo-garcia-de-sousa <sousagustavogarcia@gmail.com>
2023-02-12 18:56:33 -0300

    init

--- js/main.js
+++ js/main.js
@@ -143,7 +143,7 @@ jQuery(document).ready(function( $ ) {
           $('.time-countdown').each(function() {
           var $this = $(this), finalDate = $(this).data('countdown');
           $this.countdown(finalDate, function(event) {
-              var $this = $(this).html(event.strftime('' + '<div class="counter-column"><div class="inner"><span class="count">%D</span>Days</div></div> ' + '<div class="counter-column"><div class="inner"><span class="count">%H</span>Hours</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%M</span>Mins</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%S</span>Secs</div></div>'));
+              var $this = $(this).html(event.strftime('' + '<div class="counter-column"><div class="inner"><span class="count">%D</span>Dias</div></div> ' + '<div class="counter-column"><div class="inner"><span class="count">%H</span>Horas</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%M</span>Minutos</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%S</span>Segundos</div></div>'));
           });
        });
       }
