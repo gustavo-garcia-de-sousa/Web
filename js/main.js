89186092e67eccae99b1b9be2dabade387487321
gustavo-garcia-de-sousa <sousagustavogarcia@gmail.com>
2023-02-12 14:32:40 -0300

    init

--- js/main.js
+++ js/main.js
@@ -22,12 +22,6 @@ function reveal(){
         }
     });
 }
-window.addEventListener("scroll",reveal);
-
-
-
-
-
 
 jQuery(document).ready(function( $ ) {
 
@@ -141,6 +135,18 @@ jQuery(document).ready(function( $ ) {
       $('#header').removeClass('mobile-nav-toggle-scrolled');
     }
   });
+
+
+
+        // count down
+        if($('.time-countdown').length){  
+          $('.time-countdown').each(function() {
+          var $this = $(this), finalDate = $(this).data('countdown');
+          $this.countdown(finalDate, function(event) {
+              var $this = $(this).html(event.strftime('' + '<div class="counter-column"><div class="inner"><span class="count">%D</span>Days</div></div> ' + '<div class="counter-column"><div class="inner"><span class="count">%H</span>Hours</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%M</span>Mins</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%S</span>Secs</div></div>'));
+          });
+       });
+      }
 
   // Intro carousel
   var introCarousel = $(".carousel");
